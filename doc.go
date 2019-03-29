7b1b22617921aa8ed5ee85884778cb085ed25c68
isfonzar <icaro.isf@gmail.com>
2019-03-29 11:12:56 +0100

    Add doc.go to the root so it can included in projects

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Package pagarme is a Pagarme SDK for the Go programming language.
+//
+// The Pagarme SDK for Go provides APIs and utilities that developers can use to
+// build Go applications that use Pagarme services.
+//
+// The SDK removes the complexity of coding directly against a web service
+// interface. It hides a lot of the lower-level plumbing, such as authentication,
+// request retries, and error handling.
+package pagarme
